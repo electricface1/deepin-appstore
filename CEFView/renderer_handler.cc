b0be8c74aee660fa66fca96b5dd08af0370c7162
snyh <snyh@snyh.org>
2017-06-02 12:52:25 +0800

    Fix warning of getpid
    Change-Id: I9ba0efd6f1138a18a9155d7f65ec68481d3faea8

--- CEFView/renderer_handler.cc
+++ CEFView/renderer_handler.cc
@@ -2,7 +2,8 @@
 // reserved. Use of this source code is governed by a BSD-style license that
 // can be found in the LICENSE file.
 #include <QDebug>
-
+#include <sys/types.h>
+#include <unistd.h>
 #include "include/cef_browser.h"
 
 #include "renderer_handler.h"
